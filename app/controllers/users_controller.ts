1e348bcea7830d62ba23eb81a5d75080545735e6
bentshims <bentshims21@gmail.com>
2025-06-03 02:08:15 +0200

    creatioin de la logique metier pour l'inscription et la connexion ainsi que la rencotre du bug sur la connexxion user

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,6 +1,8 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import { signupValidator, loginvalidator } from '#validators/user'
 import User from '#models/user'
+import { messages } from '@vinejs/vine/defaults'
+import auth from '@adonisjs/auth/services/main'
 
 export default class UsersController {
   public async index({ view }: HttpContext) {
@@ -15,12 +17,30 @@ export default class UsersController {
     return view.render('pages/security/signup')
   }
 
-  public async signupUser({view}:HttpContext){
+  public async signupUser({view, request, response}:HttpContext){
+    const payload = await request.validateUsing(signupValidator)
+    if (!payload) {
+      return response.send('Nous avons rencontrer une erreur lors de la creation du compte, veillez reesayer plutard')
+    } else {
 
+      await User.create({
+        fullName : payload.fullName,
+        email : payload.email,
+        password : payload.password
+      })
     return view.render('pages/home')
+    }
   }
 
-  public async loginUser({view}:HttpContext){
-    return view.render('pages/home')
+  public async loginUser({view, request, response, auth}:HttpContext){
+    const payload = await request.validateUsing(loginvalidator)
+    if (!payload) {
+      return response.send("L'utilisateur n'existe pas veuille entrer les informations valide")
+    } else {
+      const {email, password} = payload
+      const user = await User.verifyCredentials(email, password)
+      await auth.use('web').login(user)
+      return view.render('pages/home')
+    }
   }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -8,13 +8,17 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
-router.on('/').render('pages/home')
-router.resource('/tweet', '#controllers/tweets_controller')
-router.resource('/users', '#controllers/users_controller')
+router.group(
+    ()=>{
+        router.on('/').render('pages/home')
+        router.resource('/tweet', '#controllers/tweets_controller')
+        router.resource('/users', '#controllers/users_controller')
+    }
+).use(middleware.auth())
 router.get('/login','#controllers/users_controller.login')
 router.get('/signup','#controllers/users_controller.signup')
-
 router.post('/loginUser','#controllers/users_controller.loginUser')
 router.post('/signupUser','#controllers/users_controller.signupUser')
 
