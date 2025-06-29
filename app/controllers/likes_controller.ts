5bffcd8205e395006659a67724eec9dcacb9ecc6
bentshims <bentshims21@gmail.com>
2025-06-29 23:57:42 +0200

    finissage de la creation de la logique like toggle

--- app/controllers/likes_controller.ts
+++ app/controllers/likes_controller.ts
@@ -1,4 +1,25 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import Like from '#models/like'
+import Tweet from '#models/tweet'
+import type { HttpContext } from '@adonisjs/core/http'
 
 export default class LikesController {
+
+    public async create({params, auth, response}:HttpContext){
+        const tweet = await Tweet.findOrFail(params.id) 
+        const user = auth.user!
+        const isLike = await Like.query().where('user_id', user.id).andWhere('tweet_id',tweet.id).first()
+
+        if (isLike) {
+            await isLike.delete()
+        }else{
+            await Like.create({
+                userId: user.id,
+                tweetId: tweet.id
+
+            })
+        }
+
+        return response.redirect().back()
+
+    }
 }
